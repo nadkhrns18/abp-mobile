8dcbd7cfe386f6a1a10ad5b23707549ac65244f6
nadkhrns18 <90542989+nadkhrns18@users.noreply.github.com>
2023-06-19 14:33:37 +0700

    Update HomePage.dart

--- lib/pages/asset/HomePage.dart
+++ lib/pages/asset/HomePage.dart
@@ -19,6 +19,7 @@ class _HomePageState extends State<HomePage> {
 
   readAssets() async {
     assets.clear();
+    setState(() {});
 
     Uri url = Uri.parse(
       'http://192.168.1.26/ABP_Mobile/asset/read.php',
@@ -39,6 +40,74 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  showMenuItem(AssetModel item) {
+    showDialog(
+        context: context,
+        builder: (context) {
+            return SimpleDialog(
+                title: Text(item.name),
+                children: [
+                  ListTile(
+                        onTap: () {
+                            Navigator.pop(context);
+                            Navigator.push(
+                              context, 
+                              MaterialPageRoute(
+                              builder: (context) =>  const UpdateAssetPage(
+                                oldAsset: item,
+                              ), //UpdateAssetPage
+                            ), // MaterialPageRoute
+                          ). then((value) => readAssets());
+                        },
+                        horizontalTitleGap: 0,
+                        leading: const Icon(Icon.edit, colors.blue),
+                        title: const Text('Update'),
+                   ),
+                  ListTile(
+                        onTap: () {
+                            Navigator.pop(context);
+                        },
+                        horizontalTitleGap: 0,
+                        leading: const Icon(Icon.delete, colors.red),
+                        title: const Text('Delete'),
+               ), // ListTile
+             ],
+            ); // SimpleDialog
+        },
+    )
+  }
+
+  deleteAsset(AssetModel item) async {
+    bool? yes = await DInfo.dialogConfirmation(
+        context, 
+        'Delete', 
+        'You sure want to delete ${item.name}?',
+    );
+    if (yes??false) {
+      Uri url = Uri.parse(
+        '${AppConstant.baseURL}/asset/delete,php',
+      );
+    try {
+        final response = await http.post(url, body: {
+            'id': item.id,
+            'image': item.image,
+        });
+        DMethod.printResponse(response);
+
+         Map resBody= jsonDecode(response.body);
+        bool success = resBody['success'] ?? false;
+        if (success) {
+            DInfo.toastSuccess('Success Delete Asset');
+            readAssets();//refresh data list asset
+        } else {
+            DInfo.toastError('Failed Delete Asset');
+        }
+      } catch (e) {
+        DMethod.printTittle('catch', e.toString());
+      }
+    }
+  }
+
   @override
   void initState() {
     readAssets();
@@ -49,20 +118,46 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: PopupMenuButton(
+                    icon: const Icon(Icons.menu),
+                    onSelected: (value) {
+                        if (value=='logout'){
+                            Navigator.pushReplacment(
+                        context,
+                        MaterialPageRoute(builder: (context) => Loginpage()),
+                        );     
+                        }
+                    itemBuilder:(context) => [
+                    const PopupMenuItem(
+                        value: 'logout',
+                        child: Text('Logout'),
+                ),
+              ], 
+            ), //  PopupMenuButton
         title: Text(AppConstant.appName),
         centerTitle: true,
         actions: [
           IconButton(
               onPressed: () {
-                readAssets();
-              },
-              icon: Icon(Icons.search))
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(
+                    builder: (context) =>  const SearchAssetPage(),
+                  ), // MaterialPageRoute
+                );
+                  },
+              icon: Icon(Icons.search),
+          ), // IconButton
         ],
-      ),
+      ), // AppBar
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context, MaterialPageRoute(builder: (context) => HomePage()));
+              context, 
+              MaterialPageRoute(
+                builder: (context) =>  const CreateAssetPage(),
+                ), // MaterialPageRoute
+            ). then((value) => readAssets());
         },
         child: const Icon(Icons.add),
       ),
@@ -86,6 +181,7 @@ class _HomePageState extends State<HomePage> {
                     mainAxisSpacing: 16,
                     crossAxisSpacing: 16,
                     childAspectRatio: 0.8),
+                ), // SilverGridDelegateWithFixedCrossAxisCount
                 itemBuilder: (context, index) {
                   AssetModel item = assets[index];
                   return Container(
@@ -103,11 +199,10 @@ class _HomePageState extends State<HomePage> {
                             'http://192.168.1.26/image/' + item.image,
                             fit: BoxFit.cover,
                             width: double.infinity,
-                          ),
-                        )),
-                        SizedBox(
-                          height: 8,
-                        ),
+                          ), // Image.network
+                        ), // ClipRRect
+                      ), // Expanded
+                      const SizedBox(height: 8),
                         Row(
                           children: [
                             Expanded(
@@ -135,7 +230,10 @@ class _HomePageState extends State<HomePage> {
                               borderRadius: BorderRadius.circular(4),
                               child: InkWell(
                                 borderRadius: BorderRadius.circular(4),
-                                onTap: () {},
+                                onTap: () {
+                                    showMenuItem(item);
+                                },
+                                splashColor: Colors.purpleAccent,
                                 child: Padding(
                                   padding:
                                       const EdgeInsets.symmetric(vertical: 4),
